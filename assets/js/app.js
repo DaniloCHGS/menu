38703539b8a533f9c03830077622572819c6db44
DaniloCHGS <danilochagasbm@gmail.com>
2023-03-29 19:41:47 -0300

    Mudanças na plataforma

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,27 @@
 $(document).ready(function () {
+  $("[data-scroll-top]").click(function (e) {
+    e.preventDefault();
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+  });
   $(".lightgallery").lightGallery();
+
+  function sharePage() {
+    const url = window.location.href;
+    const title = document.title;
+    const text = "Confira esta página:";
+
+    navigator
+      .share({ url, title, text })
+      .then(() => console.log("Compartilhado com sucesso!"))
+      .catch((error) => console.error("Erro ao compartilhar: ", error));
+  }
+
+  const btnShare = document.querySelector(".btn-share");
+  btnShare.addEventListener("click", sharePage);
+
+  const btnWishlist = document.querySelector(".btn-wishlist");
+  btnWishlist.addEventListener("click", (event) => {
+    event.preventDefault();
+    btnWishlist.classList.toggle("active");
+  });
 });
